41273edc2d1130110d9c4bd84caa25c0a88c15c5
Matheus Abrantes <abranteselcio@gmail.com>
2024-03-28 08:28:16 -0300

    refactored code and commented functions

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,36 +1,33 @@
-import { Controller, Get, Post, Body, Param, Delete, ConflictException} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, ConflictException } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
-import { CreateLogDto } from 'src/log/dto/create-log.dto'
-// import { LogInterceptor } from 'src/log/log.interceptor';
 
 @Controller('clients')
-export class ClientController{
+export class ClientController {
 
-  constructor(private readonly clientService: ClientService) {}
-  @Post('register')
-  // @UseInterceptors(LogInterceptor)
+  constructor(private readonly clientService: ClientService) { }
+  @Post('register') //registering clients
   async clientRegister(@Body() createClientDto: CreateClientDto) {
-  
-        const email = createClientDto.email
-        const emailExists = await this.clientService.checkEmail(email);
-        if(emailExists){
-          throw new ConflictException('Email are already exists');
-        }
-        return this.clientService.clientRegister(createClientDto);
-   
+
+    const email = createClientDto.email
+    const emailExists = await this.clientService.checkEmail(email);
+    if (emailExists) {
+      throw new ConflictException('Email are already exists');
+    }
+    return this.clientService.clientRegister(createClientDto);
+
   }
 
-  @Get('list')
+  @Get('list') //listing all clients
   findAll() {
     return this.clientService.clientList();
   }
 
-  @Get('list/:id')
+  @Get('list/:id') //listing a client by id
   findOne(@Param('id') id: string) {
     return this.clientService.clientListOne(id);
   }
-  @Delete('remove/:id')
+  @Delete('remove/:id') //removing a client by id
   remove(@Param('id') id: string) {
     return this.clientService.clientDelete(id);
   }
